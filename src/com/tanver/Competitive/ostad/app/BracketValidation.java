fbd086bccf2a63a864ef9ae267a8289d35fed293
Md. Tanver Ahammed <tanvermbstuit16@gmail.com>
2023-08-13 19:06:26 +0600

    https://ostad.app/en/dashboard/my-courses/648eb011f42b94819526c62a/module/648eb011f42b94819526c69f/assignment/648eb011f42b94819526c6a1 --> assignment solution added

---
+++ src/com/tanver/Competitive/ostad/app/BracketValidation.java
@@ -0,0 +1,26 @@
+package com.tanver.Competitive.ostad.app;
+
+import java.util.Stack;
+
+public class BracketValidation {
+
+    private boolean isValidateBracket(String brackets) {
+        Stack<Character> stackBracket = new Stack<>();
+        for (char ch : brackets.toCharArray()) {
+            if (ch == '(')
+                stackBracket.push(')');
+            else if (ch == '{')
+                stackBracket.push('}');
+            else if (ch == '[')
+                stackBracket.push(']');
+            else if (stackBracket.isEmpty() || stackBracket.pop() != ch)
+                return false;
+        }
+        return stackBracket.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BracketValidation().isValidateBracket("{}{}[]()()(){[()]}"));
+    }
+
+}
