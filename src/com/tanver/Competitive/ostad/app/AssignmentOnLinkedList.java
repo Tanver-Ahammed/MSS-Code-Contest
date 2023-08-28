524a3ece350285f42d1c353e12578cb34aa3fcf3
Md. Tanver Ahammed <tanvermbstuit16@gmail.com>
2023-08-28 13:11:47 +0600

    https://ostad.app/en/dashboard/my-courses/648eb011f42b94819526c62a/module/648eb011f42b94819526c6e7/assignment/648eb011f42b94819526c6e9 added

---
+++ src/com/tanver/Competitive/ostad/app/AssignmentOnLinkedList.java
@@ -0,0 +1,35 @@
+package com.tanver.Competitive.ostad.app;
+
+public class AssignmentOnLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+    }
+
+    public ListNode reverseLinkedListRange(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        for (int i = 0; i < left - 1; i++)
+            prev = prev.next;
+
+        ListNode curr = prev.next;
+
+        for (int i = 0; i < right - left; i++) {
+            ListNode temp = curr.next;
+            curr.next = temp.next;
+            temp.next = prev.next;
+            prev.next = temp;
+        }
+        return dummy.next;
+    }
+
+
+}
