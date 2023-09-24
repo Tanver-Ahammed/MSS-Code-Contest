7a288a27388ba5f2bc265ef921dd987981567a13
Tanver <tanvermbstuit16@gmail.com>
2023-09-24 11:40:57 +0600

    https://ostad.app/en/dashboard/my-courses/648eb011f42b94819526c62a/module/648eb012f42b94819526c777/assignment/648eb012f42b94819526c77b add

---
+++ src/com/tanver/Competitive/ostad/app/array/tree/AssignmentOnTree.java
@@ -0,0 +1,65 @@
+package com.tanver.Competitive.ostad.app.array.tree;
+
+import com.tanver.Competitive.leetcode.problems.Node;
+
+import java.util.Scanner;
+
+/**
+ * print the sub tree
+ */
+public class AssignmentOnTree {
+
+    static class Tree {
+        int data;
+        Tree left;
+        Tree right;
+
+        public Tree(int data) {
+            this.data = data;
+        }
+    }
+
+    // insert value in BST
+    private static Tree insertBST(Tree root, int data) {
+        if (root == null)
+            return new Tree(data);
+        // left sub tree
+        if (data < root.data)
+            root.left = insertBST(root.left, data);
+            // left sub tree
+        else
+            root.right = insertBST(root.right, data);
+        return root;
+    }
+
+    // sub tree
+    private static Tree subTree(Tree root, int subTreeRootVal) {
+        if (root == null || root.data == subTreeRootVal)
+            return root;
+        if (subTreeRootVal < root.data)
+            return subTree(root.left, subTreeRootVal);
+        else
+            return subTree(root.right, subTreeRootVal);
+    }
+
+    // inorder traversal
+    private static void inorder(Tree root) {
+        if (root == null)
+            return;
+        System.out.print(root.data + " ");
+        inorder(root.left);
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Tree root = null;
+        for (int i = 0; i < n; i++)
+            root = insertBST(root, scanner.nextInt());
+        int subTreeRootVal = scanner.nextInt();
+        Tree subTreeRoot = subTree(root, subTreeRootVal);
+        inorder(subTreeRoot);
+    }
+
+}
